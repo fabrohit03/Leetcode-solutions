621edb9c77c79f5b681fc6239f92fd733b25b1ec
Rohit Chandel <rohitchandel2024@gmail.com>
2026-02-02 10:08:36 +0530

    Time: 15 ms (35.93%) | Memory: 47.3 MB (12.14%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,23 +1,21 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer> need=new HashMap<>();
-        HashMap<Character,Integer> have = new HashMap<>();
-        int n= ransomNote.length();
-        int m= magazine.length();
-        for (int i=0;i<n;i++){
-            char ch =ransomNote.charAt(i);
-            need.put(ch,need.getOrDefault(ch,0)+1);
-             }
-
-        for(int j=0;j<m;j++){
-            char ch =magazine.charAt(j);
-            have.put(ch,have.getOrDefault(ch,0)+1);
-        }
-        for(char ch : need.keySet()){
-            if(have.getOrDefault(ch,0)<need.get(ch)){
-                return false;
-            }
-        }
-        return true;
+    HashMap<Character,Integer> have =new HashMap<>();    
+    HashMap<Character,Integer> need =new HashMap<>();  
+    int n=ransomNote.length();
+    int m=magazine.length();
+    for(int i=0;i<n;i++){
+        char ch=ransomNote.charAt(i);
+       need.put(ch,need.getOrDefault(ch,0)+1);
+    } 
+    for(int j=0;j<m;j++){
+        char ch=magazine.charAt(j);
+         have.put(ch,have.getOrDefault(ch,0)+1);
     }
-}
+    for(char ch:need.keySet()){
+        if(have.getOrDefault(ch,0)<need.get(ch)){
+            return false;
+        }
+    }
+return true;
+}}
