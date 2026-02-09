0f701eafcc7e5bcb92885c6e0aad07e66047569b
Rohit Chandel <rohitchandel2024@gmail.com>
2026-02-09 22:49:05 +0530

    Time: 21 ms (91.58%) | Memory: 56.4 MB (32.93%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -0,0 +1,35 @@
+class Pair{
+    int x;
+    int y;
+    Pair(int x , int y){
+       this.x=x;
+       this.y=y;
+
+    }
+
+
+int dist(){
+    return x*x+y*y;
+}
+}
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<Pair> pq=new PriorityQueue <>(
+            (a,b)->a.dist()-b.dist()
+        );
+
+        for(int[] p:points){
+            pq.offer(new Pair(p[0],p[1]));
+        }
+
+        int [][] res= new int [k][2];
+        for(int i=0;i<k;i++){
+            Pair curr=pq.poll();
+            res[i][0]=curr.x;
+            res[i][1]=curr.y;
+        }
+        return res;
+
+    }
+}
