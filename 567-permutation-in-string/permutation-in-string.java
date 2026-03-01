2b306c86b591a97a71671c03950ab9c865c2ebdb
Rohit Chandel <rohitchandel2024@gmail.com>
2026-03-01 10:17:53 +0530

    Time: 5 ms (96.96%) | Memory: 43.9 MB (87.92%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,29 @@
+class Solution {
+      private boolean matches(int [] a ,int [] b){
+        for(int i=0;i<26;i++){
+            if(a[i]!=b[i]) return false;
+        }
+        return true;
+    }
+    public boolean checkInclusion(String s1, String s2) {
+    int n = s1.length();
+    int m = s2.length();
+    if(n>m) return false;
+    int [] count1 = new int [26];
+    int [] count2 = new int [26];
+    for(char ch : s1.toCharArray()){
+        count1[ch-'a']++;
+    }
+    for(int i=0 ; i<n ; i++){
+        count2[s2.charAt(i)-'a']++;
+        if(matches(count1,count2)) return true;
+    }
+    for(int i=n ; i<m ; i++){
+        count2[s2.charAt(i)-'a']++;
+        count2[s2.charAt(i-n)-'a']--;
+        if(matches(count1,count2)) return true;
+    }
+  
+    return false;
+    }
+}
