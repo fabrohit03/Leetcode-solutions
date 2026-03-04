7162b836daca250e81bceb792b1a11ce115f252f
Rohit Chandel <rohitchandel2024@gmail.com>
2026-03-04 08:28:38 +0530

    Time: 5 ms (95.27%) | Memory: 197.9 MB (39.78%) - LeetSync

--- 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.java
+++ 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.java
@@ -1,44 +1,38 @@
 class Solution {
     public ListNode reverseEvenLengthGroups(ListNode head) {
-        ListNode dummy = new ListNode(0);
-        dummy.next=head;
-        ListNode curr=head;
-        ListNode prev=dummy;
-
-        int groupsize =1;
-        while(curr!=null){
-            ListNode temp = curr;
-            int count =0;
-            while(temp!=null && count<groupsize){
-                temp=temp.next;
-                count++;
-            }
-
-            if(count%2==0){
-                ListNode node=curr;
-                ListNode prevNode = null;
-                for(int i=0;i<count;i++){
-                    ListNode next=node.next;
-                    node.next=prevNode;
-                    prevNode=node;
-                    node =next;
-                }
-
-                prev.next=prevNode;
-                curr.next=node;
-
-                prev=curr;
-                curr=node;
-
-            }
-            else{
-                for(int i=0;i<count;i++){
-                    prev=curr;
-                    curr=curr.next;
-                }
-            }
-            groupsize++;
+      ListNode dummy = new ListNode(0);
+      dummy.next =head;
+      ListNode curr = head;
+      ListNode prev = dummy;
+      int groupSize = 1;
+      while(curr!=null){
+        ListNode temp = curr;
+        int count = 0 ;
+        while(temp!=null && count<groupSize){
+            temp = temp.next;
+            count++;
         }
-        return dummy.next;
+        if(count%2==0){
+            ListNode node = curr;
+            ListNode prevNode = null;
+            for(int i=0;i<count;i++){
+                ListNode next = node.next;
+                node.next = prevNode;
+                prevNode = node;
+                node = next;
+            }
+            prev.next = prevNode;
+            curr.next = node;
+            prev = curr;
+            curr = node;
+        }else{
+            for(int i=0;i<count;i++){
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+        groupSize++;
+      }
+      return dummy.next;
     }
 }
