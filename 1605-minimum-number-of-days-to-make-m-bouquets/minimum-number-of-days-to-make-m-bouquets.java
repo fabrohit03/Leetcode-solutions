dc1ebd2f9a2f63c79228ea4a5aeac1d525016d7e
Rohit Chandel <rohitchandel2024@gmail.com>
2026-03-05 07:14:49 +0530

    Time: 21 ms (27.62%) | Memory: 82.6 MB (64.69%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,39 +1,39 @@
 class Solution {
-    private boolean bloom(int bloomDay[],long day,int m,int k){
-        int count =0;
-        int bouquets=0;
+    private boolean bloom(int [] bloomDay, long guess, int m ,int k){
+        int count = 0;
+        int bouquet = 0;
         for(int i=0;i<bloomDay.length;i++){
-            if(bloomDay[i]<=day){
+            if(bloomDay[i]<=guess){
                 count++;
-                 if(count==k){
-                    bouquets++;
-                    count=0;
+                if(count ==k){
+                    bouquet++;
+                    count =0;
                 }
             }else{
-               count=0;
+                count =0;
             }
-            
         }
-        return bouquets>=m;
+        return bouquet>=m;
     }
+
     public int minDays(int[] bloomDay, int m, int k) {
-        if(bloomDay.length<m*k) return -1;
-        long low=Integer.MAX_VALUE;
-        long high=Integer.MIN_VALUE;
-        for(int day:bloomDay){
-            low=Math.min(low,day);
-            high=Math.max(high,day);
+        if(bloomDay.length<(long) m*k) return -1;
+        long low = Integer.MAX_VALUE;
+        long high = Integer.MIN_VALUE;
+        for(int day : bloomDay){
+            low = Math.min(low,day);
+            high = Math.max(high,day);
         }
-        long ans=-1;
-        
+        long ans = -1;
         while(low<=high){
-            long guess=(low+high)/2;
+            long guess = low+(high-low)/2;
             if(bloom(bloomDay,guess,m,k)==true){
-                ans=guess;
-                high=guess-1;
+                ans = guess;
+                high = guess-1;
             }else{
-                low=guess+1;
-            }}
+                low = guess+1;
+            }
+        }
         return (int) ans;
     }
 }
