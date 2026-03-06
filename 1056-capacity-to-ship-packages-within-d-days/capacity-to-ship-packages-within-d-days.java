d41cd88e3b4041b2933ebbff3d36b80f6b765b49
Rohit Chandel <rohitchandel2024@gmail.com>
2026-03-06 07:52:40 +0530

    Time: 12 ms (30.43%) | Memory: 50.4 MB (35.16%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,35 +1,36 @@
 class Solution {
-    private boolean func(int [] weights , long guess,int days){
-        int count =1;
-        int sum=0;
+    private boolean day(int [] weights,long guess,int days){
+        int count = 1;
+        int sum =0;
         for(int i=0;i<weights.length;i++){
             if(sum+weights[i]>guess){
                 count++;
                 sum=weights[i];
             }
-            else {
+            else{
                 sum+=weights[i];
             }
         }
         return count<=days;
     }
     public int shipWithinDays(int[] weights, int days) {
-        long low=0;
-        long high = 0;
-        for(int w:weights){
-            low=Math.max(low,w);
-            high+=w;
+     long low =0;
+     long high=0;
+     long res =0;
+     for(int w:weights){
+        low = Math.max(low,w);
+        high+=w;
+     }
+     while(low<=high){
+        long guess = low+(high-low)/2;
+        if(day(weights,guess,days)==true){
+            res=guess;
+            high = guess-1;
         }
-        long res=0;
-        while(low<=high){
-            long guess = low+(high-low)/2;
-            if(func(weights,guess,days)==true){
-                res=guess;
-                high=guess-1;
-            }else{
-                low=guess+1;
-            }
+        else{
+            low = guess+1;
         }
-        return (int) res;
+     }
+     return (int) res;
     }
 }
