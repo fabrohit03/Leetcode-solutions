fc602a1c6a19e58f90268fae4fe8fcd1d7365f2f
Rohit Chandel <rohitchandel2024@gmail.com>
2026-03-14 10:05:30 +0530

    Time: 1 ms (85.19%) | Memory: 45.6 MB (43.36%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,27 +1,31 @@
 class Solution {
 
-    void func(int [] nums ,boolean [] used, int n ,List<List<Integer>>res , List<Integer>temp){
-        if(n==temp.size()){
-            res.add(new ArrayList<>(temp));
-            return ;
-        }
-        for(int i=0;i<n;i++){
-            if(used[i]) continue;
-            used[i]=true;
+    void func(int index,int [] nums ,List<List<Integer>>res ){
+        if(index==nums.length){
+            List <Integer> temp = new ArrayList<>();
+            for(int i=0;i<nums.length;i++){
             temp.add(nums[i]);
-            func(nums,used,n,res,temp);
-            temp.remove(temp.size()-1);
-            used[i]=false;
-
         }
+        res.add(new ArrayList<>(temp));
+        return ;
+        }
+        for(int i=index;i<nums.length;i++){
+            swap(i,index,nums);
+            func(index+1,nums,res);
+            swap(i,index,nums);
+        }
+        }
+        
+
+    private void swap(int i, int j, int [] nums){
+        int t = nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
     }
 
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        int n = nums.length;
-        boolean [] used = new boolean[nums.length];
-        func(nums,used,n,res,temp);
+        func(0,nums,res);
         return res;
     }
 }
