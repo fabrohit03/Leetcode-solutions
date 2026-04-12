2381c19107dd8c47a9b9d06a8110b88c71142444
Rohit Chandel <rohitchandel2024@gmail.com>
2026-04-12 09:17:41 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (31.99%) - LeetSync

---
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -0,0 +1,14 @@
+class Solution {
+    int pre_idx=0;
+    int post_idx=0;
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        TreeNode root = new TreeNode(preorder[pre_idx]);
+        pre_idx++;
+        if(root.val!=postorder[post_idx])
+        root.left=constructFromPrePost(preorder,postorder);
+        if(root.val!=postorder[post_idx])
+        root.right=constructFromPrePost(preorder,postorder);
+        post_idx++;
+        return root;
+    }
+}
