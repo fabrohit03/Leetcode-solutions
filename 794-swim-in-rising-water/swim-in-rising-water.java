c6d43e855326ccc20a77716d885053bd5fae58cf
Rohit Chandel <rohitchandel2024@gmail.com>
2026-05-18 08:51:11 +0530

    Time: 9 ms (85.54%) | Memory: 46.3 MB (59.99%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -1,51 +1,47 @@
 class Solution {
+    class Pair{
+        int row;
+        int col;
+        int dist;
+        Pair(int row, int col, int dist){
+            this.row = row;
+            this.col = col;
+            this.dist = dist;
+        }
+    }
     public int swimInWater(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int low = grid[0][0];
-        int high = grid[0][0];
-        for(int i =0; i<n ;i++){
-            for(int j =0 ; j<m; j++){
-                high = Math.max(high,grid[i][j]);
-            }
+        int [][] res = new int [n][m];
+        for(int i=0; i<n;i++){
+            Arrays.fill(res[i],Integer.MAX_VALUE);
         }
-        int res = high;
-        while(low<=high){
-            int guess = (low+high)/2;
-            if(bfs(grid,guess)){
-                res = guess;
-                high = guess-1;
-            }else{
-                low = guess+1;
+        PriorityQueue<Pair>pq = new PriorityQueue<>((a,b)->a.dist-b.dist);
+        int[] x = {1,-1,0,0};
+        int[] y = {0,0,1,-1};
+        res[0][0] = grid[0][0];
+        pq.offer(new Pair(0,0,grid[0][0]));
+        while(!pq.isEmpty()){
+            Pair p = pq.poll();
+            int row = p.row;
+            int col = p.col;
+            int dis = p .dist;
+            if(row==n-1 && col==m-1){
+                return dis;
             }
-            
-        }
-       
-        return res;
-    }
-     private boolean bfs(int [][] grid,int money){
-            int n = grid.length;
-            int m = grid[0].length;
-            if(grid[0][0]>money) return false;
-            int [][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-            boolean [][] visited = new boolean[n][n];
-            Queue<int[]>q = new LinkedList<>();
-            q.offer(new int[]{0,0});
-            visited[0][0]=true;
-            while(!q.isEmpty()){
-                int [] cur = q.poll();
-                int x = cur[0];
-                int y = cur[1];
-                if(x==n-1 && y==m-1) return true;
-                for(int[] d : dirs){
-                    int nx =x+d[0];
-                    int ny = y+d[1];
-                    if(nx>=0 && ny>=0 && nx<n && ny<m && !visited[nx][ny] && grid[nx][ny]<=money){
-                        visited[nx][ny]=true;
-                        q.offer(new int[]{nx,ny});
-                    }
+            if(dis>res[row][col]) continue;
+            for(int k=0;k<4;k++){
+                int r = row+x[k];
+                int c = col+y[k];
+                if(r<0 || c<0 ||r >=n || c>=m) continue;
+                int newWt = Math.max(dis,grid[r][c]);
+                if(newWt<res[r][c]){
+                    res[r][c]= newWt;
+                    pq.offer(new Pair(r,c,newWt));
                 }
             }
-            return false;
         }
+        return 0;
+    }
+     
 }
