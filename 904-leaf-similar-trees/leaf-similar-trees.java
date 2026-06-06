6c55794ad0597423ab45f82420b4cabdf078fc36
Rohit Chandel <rohitchandel2024@gmail.com>
2026-06-06 10:45:19 +0530

    Time: 1 ms (18.04%) | Memory: 43.1 MB (69.41%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.java
+++ 904-leaf-similar-trees/leaf-similar-trees.java
@@ -1,17 +1,23 @@
 class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        List<Integer>l1 = new ArrayList<>();
-        List<Integer>l2 = new ArrayList<>();
-        dfs(root1,l1);
-        dfs(root2,l2);
-        return l1.equals(l2);
+       Stack<TreeNode>st1 = new Stack<>();
+       Stack<TreeNode>st2 = new Stack<>();
+       st1.push(root1);
+       st2.push(root2);
+       while(!st1.isEmpty() && !st2.isEmpty()){
+        int l1 = dfs(st1);
+        int l2 = dfs(st2);
+        if(l1!=l2) return false;
+        
+       }
+       return st1.isEmpty() && st2.isEmpty();
     }
-    private void dfs(TreeNode root,List<Integer>leaves){
-        if(root==null) return;
-        dfs(root.left,leaves);
-        dfs(root.right,leaves);
-        if(root.left==null && root.right==null) {
-            leaves.add(root.val);
-            return;}
+    private int dfs(Stack<TreeNode>stack){
+        while(true){
+       TreeNode node = stack.pop();
+       if(node.left!=null) stack.push(node.left);
+       if(node.right!=null) stack.push(node.right);
+       if(node.left==null && node.right==null) return node.val;
+    }
     }
 }
