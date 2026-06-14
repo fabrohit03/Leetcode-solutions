77506a438bb0241b00730369d208dd44f70f99ff
Rohit Chandel <rohitchandel2024@gmail.com>
2026-06-14 07:53:10 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (69.01%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,13 +1,15 @@
 class Solution {
-    Map<Integer,Integer> map = new HashMap<>();
     public int fib(int n) {
         if(n==0 || n==1) return n;
-        if(map.containsKey(n)){
-            return map.get(n);
+        int prev = 1;
+        int prev2 = 0;
+        for(int i=2; i<=n; i++){
+            int ans = prev+prev2;
+            prev2=prev;
+            prev=ans;
+            
         }
-        int ans = fib(n-1)+fib(n-2);
-        map.put(n,ans);
-        return ans;
+        return prev;
     }
     
 }
