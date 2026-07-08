d8ad7c42d02278f9cb973b62915942fe2d58be7c
Rohit Chandel <rohitchandel2024@gmail.com>
2026-07-08 21:15:33 +0530

    Time: 19 ms (91.39%) | Memory: 53.4 MB (89.67%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -2,21 +2,17 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int n = text1.length();
         int m = text2.length();
-        int [] [] dp = new int[n][m];
-        for(int[] row: dp){
-            Arrays.fill(row,-1);
+        int [][] dp = new int [n+1] [m+1];
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                if(text1.charAt(i)==text2.charAt(j)){
+                    dp[i][j]=1+dp[i+1][j+1];
+                }else{
+                    dp[i][j] =Math.max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
         }
-        return func(text1,text2,n,m,0,0,dp);
+        return dp[0][0];
     }
     
-    private int func(String text1, String text2, int n , int m , int i, int j,int [][] dp){
-        if(i==n || j==m) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(text1.charAt(i)==text2.charAt(j)){
-            return dp[i][j] = 1+func(text1,text2,n,m,i+1,j+1,dp);
-        }
-        int c1 = func(text1,text2,n,m,i,j+1,dp);
-        int c2 = func(text1,text2,n,m,i+1,j,dp);
-        return dp[i][j]=Math.max(c1,c2);
-    }
 }
