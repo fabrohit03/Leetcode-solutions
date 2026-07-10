a964f4d32414d22d5ebc2dab853d9c140298b576
Rohit Chandel <rohitchandel2024@gmail.com>
2026-07-10 20:35:08 +0530

    Time: 20 ms (1.40%) | Memory: 92.9 MB (97.42%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,27 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int [] [] dp = new int [n][3];
-        for(int [] rows: dp){
-            Arrays.fill(rows,-1);
+        int [] [] dp = new int[n+1][3];
+        for(int i = n-1; i>=0; i--){
+            dp[i][2] = Math.max(dp[i+1][1]-prices[i],dp[i+1][2]);
+            dp[i][1] = Math.max(dp[i+1][0]+prices[i],dp[i+1][1]);
         }
-        
-        int k =2;
-        return func(prices,n,0,k,dp);
-    }
-    private int func(int[] prices, int n , int i , int k, int[] [] dp){
-        if(i==n) return 0;
-        if(k==0) return 0;
-        if(dp[i][k]!=-1) return dp[i][k];
-        if(k==2){
-            int c1 = func(prices,n,i+1,k-1,dp)-prices[i];
-            int c2 = func(prices,n,i+1,k,dp);
-            return dp[i][k]=Math.max(c1,c2);
-        }else{
-            int c1 = func(prices,n,i+1,k-1,dp)+prices[i];
-            int c2 = func(prices,n,i+1,k,dp);
-            return dp[i][k]=Math.max(c1,c2);
-        }
-    
+        return dp[0][2];
     }
 }
