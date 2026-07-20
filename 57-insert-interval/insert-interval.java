061c2559b7b8d3e5058d658ee2cafb87990fe62e
Rohit Chandel <rohitchandel2024@gmail.com>
2026-07-20 10:04:15 +0530

    Time: 1 ms (98.01%) | Memory: 46.8 MB (94.91%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,37 +1,34 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-     List <int[]> list= new ArrayList<>();
-     int n=intervals.length;
-     boolean insert=false;
-     for(int i=0;i<n;i++){
-        int start=intervals[i][0];
-        if(insert==false && start>=newInterval[0]){
-            list.add(newInterval);
-            insert =true;
+        List<int[]> list = new ArrayList<>();
+        int n = intervals.length;
+        boolean insert = false;
+        for(int i=0; i<n ; i++){
+            int st = intervals[i][0];
+            if(st>=newInterval[0] && insert==false){
+                list.add(newInterval);
+                insert = true;
+            }
+            list.add(intervals[i]);}
+            if(!insert){
+                list.add(newInterval);
+            }
+        List<int[]> res = new ArrayList<>();
+        int st1 = list.get(0)[0];
+        int end1 = list.get(0)[1];
+        for(int i=0; i<list.size();i++){
+            int st2 = list.get(i)[0];
+            int end2 = list.get(i)[1];
+            if(end1>=st2){
+                st1 = st1;
+                end1 = Math.max(end1,end2);
+                continue;
+            }
+            res.add(new int[]{st1,end1});
+            st1 = st2;
+            end1 = end2;
         }
-        list.add(intervals[i]);
-
-     }
-     if(!insert){
-        list.add(newInterval);
-     }
-
-     List <int []> res= new ArrayList<>();
-     int start1=list.get(0)[0];
-     int end1=list.get(0)[1];
-     for(int i=1;i<list.size();i++){
-        int start2=list.get(i)[0];
-        int end2=list.get(i)[1];
-        if(end1>=start2){
-            start1=start1;
-            end1=Math.max(end1,end2);
-            continue;
-        }
-        res.add(new int [] {start1,end1});
-        start1=start2;
-         end1=end2;
-     }
-     res.add(new int[]{start1,end1});
-     return res.toArray(new int[res.size()][]);
+        res.add(new int[]{st1,end1});
+        return res.toArray(new int[res.size()][]);
     }
 }
