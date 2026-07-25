4ac956a27b919b36f9bd303ac40ab63169df45dd
Rohit Chandel <rohitchandel2024@gmail.com>
2026-07-25 09:57:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (12.21%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -1,35 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-
-    ListNode reverse(ListNode head , int size){
+    ListNode reverse(ListNode head, int size){
         ListNode curr = head;
         ListNode prev = null;
-        while(size-- > 0){
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+        while(size-- >0){
+            ListNode next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
         }
         return prev;
     }
-
     public ListNode swapPairs(ListNode head) {
-
         ListNode left = head;
-        ListNode right;
+        ListNode res = null;
         ListNode prevLeft = null;
         int size = 2;
-        ListNode res = null;
-
+        ListNode right;
         while(left!=null && left.next!=null){
-            right =left;
-            for(int i=0;i<(size-1);i++){
+            right=left;
+            for(int i=0; i<size-1; i++){
                 if(right==null) break;
                 right=right.next;
             }
-
             if(right!=null){
-                ListNode nextLeft =right.next;
-                ListNode newHead =reverse(left,size);
+                ListNode nextLeft = right.next;
+                ListNode newHead = reverse(left,size);
                 if(prevLeft!=null){
                     prevLeft.next=newHead;
                 }
@@ -38,7 +43,7 @@ class Solution {
                     res=newHead;
                 }
                 left.next=nextLeft;
-                left=nextLeft;
+                left = nextLeft;
             }
         }
         return res!=null ? res : head;
