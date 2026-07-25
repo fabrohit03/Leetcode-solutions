a3c09d831b602ef3a98870a80138a47078d4a1d8
Rohit Chandel <rohitchandel2024@gmail.com>
2026-07-25 10:18:52 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (17.06%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -1,45 +1,40 @@
 class Solution {
-
-    ListNode reverse(ListNode head,int size){
-        ListNode curr=head;
-        ListNode prev=null;
-        while(size-- >0){
-            ListNode next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
-        }
-        return prev;
-    }
-
-    public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode left=head;
-        ListNode res=null;
-        ListNode prevLeft=null;
-       
-        int size=k;
-     while(true){
-         ListNode right=left;
-        for(int i=0;i<k;i++){
-            if(right==null){
-                return res!=null ? res: head;
+    ListNode reverse(ListNode head, int size){
+            ListNode curr = head;
+            ListNode prev = null;
+            while(size-- >0){
+                ListNode next = curr.next;
+                curr.next=prev;
+                prev=curr;
+                curr=next;
             }
-            right=right.next;
+            return prev;
         }
-
-        ListNode nextLeft=right;
-        ListNode newHead= reverse(left,size);
-        if(prevLeft!=null){
-            prevLeft.next=newHead;
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode left = head;
+        ListNode res = null;
+        ListNode prevLeft = null;
+        int size = k;
+        ListNode right;
+        while(true){
+            right=left;
+            for(int i=0; i<k; i++){
+                if(right==null){
+                    return res!=null ? res : head;
+                }
+                right=right.next;
+            }
+                ListNode nextLeft = right;
+                ListNode newHead = reverse(left,size);
+                if(prevLeft!=null){
+                    prevLeft.next = newHead;
+                }
+                prevLeft = left;
+                 if(res==null){
+                     res=newHead;
+                }
+                left.next=nextLeft;
+                left=nextLeft;
         }
-        prevLeft=left;
-        if(res==null){
-            res=newHead;
-        }
-        left.next=nextLeft;
-        left=nextLeft;
-        
     }
-    }
-      
 }
