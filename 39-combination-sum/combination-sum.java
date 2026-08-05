45cb5363c907e1864593c02a5047c04fed5b6d4b
Rohit Chandel <rohitchandel2024@gmail.com>
2026-08-05 16:44:05 +0530

    Time: 2 ms (96.30%) | Memory: 45.4 MB (96.54%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,27 +1,24 @@
 class Solution {
-
-    void func(int [] candidates,int n,int sum , int index,int target , List<List<Integer>>res,List<Integer>diary){
+    void func(int[] candidates, int n, int sum, int target, int index, List<List<Integer>>res, List<Integer>diary){
         if(index==n){
             if(target==sum)
-            res.add(new ArrayList<>(diary));
+                res.add(new ArrayList<>(diary));
             return;
         }
-
-        func(candidates,n,sum,index+1,target,res,diary);
-        if(candidates[index]+sum<=target){
+        func(candidates, n, sum, target, index+1, res,diary);
+        if(sum+candidates[index]<=target){
             diary.add(candidates[index]);
-            sum=candidates[index]+sum;
-            func(candidates,n,sum,index,target,res,diary);
+            sum +=candidates[index];
+            func(candidates, n,sum, target, index,res, diary);
             diary.remove(diary.size()-1);
+            return;
         }
-    return ;
     }
-
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> diary = new ArrayList<>();
+        List<List<Integer>>res = new ArrayList<>();
+        List<Integer>diary = new ArrayList<>();
         int n = candidates.length;
-        func(candidates,n,0,0,target,res,diary);
+        func(candidates,n,0,target,0,res,diary);
         return res;
     }
 }
