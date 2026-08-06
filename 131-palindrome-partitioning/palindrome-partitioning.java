09bd10641412d29f569c601784afdbceb079bdc2
Rohit Chandel <rohitchandel2024@gmail.com>
2026-08-06 14:14:56 +0530

    Time: 8 ms (73.85%) | Memory: 65.1 MB (75.69%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,35 +1,30 @@
 class Solution {
-
-    boolean isPalindrome(String s, int l ,int r){
+    boolean isPalindrome(String s, int l, int r){
         while(l<r){
-            if(s.charAt(l)!=s.charAt(r)){
-                return false;
-            }
+            if(s.charAt(l)!=s.charAt(r)) return false;
             l++;
             r--;
         }
         return true;
     }
-
-    void func(String s , int index , int n , List<List<String>>res ,List<String>temp){
+    void func(String s, int index, List<List<String>>res,List<String>temp,int n){
         if(index==n){
             res.add(new ArrayList<>(temp));
             return;
         }
-      for(int i=index;i<n;i++){
-        if(isPalindrome(s,index,i)){
-            temp.add(s.substring(index,i+1));
-            func(s,i+1,n,res,temp);
-            temp.remove(temp.size()-1);
+        for(int i=index; i<n; i++){
+            if(isPalindrome(s,index,i)){
+                temp.add(s.substring(index,i+1));
+                func(s,i+1,res,temp,n);
+                temp.remove(temp.size()-1);
+            }
         }
-      }
     }
-
     public List<List<String>> partition(String s) {
-        List<List<String>> res = new ArrayList<>();
-        List<String> temp = new ArrayList<>();
+        List<List<String>>res = new ArrayList<>();
+        List<String>temp = new ArrayList<>();
         int n = s.length();
-        func(s,0,n,res,temp);
+        func(s,0,res,temp,n);
         return res;
     }
 }
