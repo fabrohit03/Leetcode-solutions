19cf19f4c0abd1816ad30e535a3d8c5589881324
Rohit Chandel <rohitchandel2024@gmail.com>
2026-08-08 10:18:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (93.54%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -1,3 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     void func(TreeNode root, List<Integer>res){
         if(root==null) return;
@@ -6,7 +21,7 @@ class Solution {
         func(root.right,res);
     }
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
+        List<Integer>res = new ArrayList<>();
         func(root,res);
         return res;
     }
