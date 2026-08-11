e8e95178feb2859b4453fca87a38d775807e002d
Rohit Chandel <rohitchandel2024@gmail.com>
2026-08-11 17:18:04 +0530

    Time: 2 ms (98.21%) | Memory: 47.2 MB (55.40%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -1,23 +1,23 @@
 class Solution {
-    List<Integer> twoSum = new ArrayList<>();
-    private void func(TreeNode root){
-        if(root==null) return;
+    List<Integer>temp = new ArrayList<>();
+
+    void func(TreeNode root){
+        if(root==null){
+            return;
+        }
         func(root.left);
-        twoSum.add(root.val);
+        temp.add(root.val);
         func(root.right);
     }
     public boolean findTarget(TreeNode root, int k) {
         func(root);
         int i =0;
-        int j = twoSum.size()-1;
+        int j = temp.size()-1;
         while(i<j){
-            long sum = twoSum.get(i)+twoSum.get(j);
+            int sum = temp.get(i)+temp.get(j);
             if(sum==k) return true;
-            else if(sum<k) {
-                i++;
-            }else{
-                j--;
-            }
+            if (sum<k) i++;
+            else j--;
         }
         return false;
     }
