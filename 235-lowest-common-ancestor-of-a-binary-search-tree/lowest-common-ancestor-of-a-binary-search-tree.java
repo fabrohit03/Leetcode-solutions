f23942a396d03d5408c2b83a35a16c0ff9c674a4
Rohit Chandel <rohitchandel2024@gmail.com>
2026-08-11 09:45:55 +0530

    Time: 6 ms (96.92%) | Memory: 47.8 MB (16.76%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,14 +1,12 @@
 class Solution {
     TreeNode ans = null;
-    private void func(TreeNode root , TreeNode p, TreeNode q){
-        if(root==null) {
-            return;
-        }
-        if(root.val<p.val && q.val>root.val) func(root.right,p,q);
-        else if (root.val>p.val && root.val>q.val) func(root.left,p,q);
+    void func(TreeNode root, TreeNode p , TreeNode q){
+        if(root==null) return;
+        if(p.val>root.val && q.val>root.val) func(root.right,p,q);
+        else if(p.val<root.val && q.val<root.val) func(root.left,p,q);
         else{
-            ans=root;
-            return ;
+            ans = root;
+            return;
         }
     }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
