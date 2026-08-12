b07896bcdf3e6f1bf6d8bc95f8c5f5a064f176fd
Rohit Chandel <rohitchandel2024@gmail.com>
2026-08-12 09:38:51 +0530

    Time: 3 ms (72.86%) | Memory: 47.2 MB (72.97%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -1,24 +1,46 @@
 class Solution {
-    List<Integer>temp = new ArrayList<>();
-
-    void func(TreeNode root){
-        if(root==null){
-            return;
+    Stack<TreeNode>asc = new Stack<>();
+    Stack<TreeNode>desc = new Stack<>();
+    TreeNode getsmall(){
+        if(asc.isEmpty()) return null;
+        TreeNode small = asc.pop();
+        TreeNode rightChild = small.right;
+        while(rightChild != null){
+            asc.push(rightChild);
+            rightChild = rightChild.left;
         }
-        func(root.left);
-        temp.add(root.val);
-        func(root.right);
+        return small;
+    }
+    TreeNode getbig(){
+        if(desc.isEmpty()) return null;
+        TreeNode big = desc.pop();
+        TreeNode leftChild = big.left;
+        while(leftChild !=null){
+            desc.push(leftChild);
+            leftChild = leftChild.right;
+        }
+        return big;
     }
     public boolean findTarget(TreeNode root, int k) {
-        func(root);
-        int i =0;
-        int j = temp.size()-1;
-        while(i<j){
-            int sum = temp.get(i)+temp.get(j);
-            if(sum==k) return true;
-            if (sum<k) i++;
-            else j--;
-        }
-        return false;
+       if(root==null) return false;
+       TreeNode t = root;
+       while(t!=null){
+        asc.push(t);
+        t=t.left;
+       }
+       t = root;
+       while(t!=null){
+        desc.push(t);
+        t=t.right;
+       }
+       TreeNode i = getsmall();
+       TreeNode j = getbig();
+       while(i!=null && j!=null && i!=j&& i.val<j.val){
+        int sum = i.val+j.val;
+        if(sum==k) return true;
+        if(sum>k) j=getbig();
+        else i = getsmall();
+       }
+       return false;
     }
 }
