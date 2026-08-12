27ca3747fe63448548bc5897db1b95da601593a5
Rohit Chandel <rohitchandel2024@gmail.com>
2026-08-12 11:37:07 +0530

    Time: 4 ms (71.28%) | Memory: 82.1 MB (56.38%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
@@ -1,5 +1,5 @@
 class Solution {
-    private int func(TreeNode root){
+    int func(TreeNode root){
         if(root==null) return 0;
         if(root.left==null && root.right==null) return 1;
         if(root.left==null) return 1+func(root.right);
