ec06dfcc116cbe034609a9074756ad96cdbeea16
Rohit Chandel <rohitchandel2024@gmail.com>
2026-08-12 10:04:04 +0530

    Time: 1 ms (30.77%) | Memory: 45.8 MB (23.16%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,11 +1,11 @@
 class Solution {
     boolean ans = true;
-    private int func(TreeNode root){
+    int func(TreeNode root){
         if(root==null) return 0;
-        int left = func(root.left);
-        int right = func(root.right);
-        if(Math.abs(left-right)>1) ans = false;
-        return 1+Math.max(left,right);
+        int l = func(root.left);
+        int r = func(root.right);
+        if(Math.abs(l-r)>1) ans = false;
+        return 1+Math.max(l,r);
     }
     public boolean isBalanced(TreeNode root) {
         func(root);
