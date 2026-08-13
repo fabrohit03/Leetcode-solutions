ca024fab79d3778953146006ab48fa7c5c96b070
Rohit Chandel <rohitchandel2024@gmail.com>
2026-08-13 10:49:50 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (94.41%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -1,42 +1,39 @@
 class Solution {
-    TreeNode galat1_first=null;
-    TreeNode galat1_sec=null;
-    TreeNode galat2_first=null;
-    TreeNode galat2_sec=null;
-    int galat=0;
+    TreeNode galat1_first = null;
+    TreeNode galat1_second = null;
+    TreeNode galat2_first = null;
+    TreeNode galat2_second = null;
     TreeNode prev = null;
-    private void func(TreeNode root){
+    int galat = 0;
+    
+    void func(TreeNode root){
         if(root==null) return;
         func(root.left);
-            if(prev!=null && root.val<prev.val){
-                
-                    if(galat==0){
-                        galat1_first=prev;
-                        galat1_sec=root;
-                        galat++;
-                    }else{
-                        galat2_first=prev;
-                        galat2_sec=root;
-                        galat++;
-                    }
-                   
-                
+        if(prev != null && root.val <=prev.val){
+            if(galat == 0){
+                galat1_first = prev;
+                galat1_second = root;
+                galat++;
             }
-            prev=root;
-            func(root.right);
+            else{
+                galat2_first = prev;
+                galat2_second = root;
+                galat++;
+            }
+        }
+        prev = root;
+        func(root.right);
     }
-
     public void recoverTree(TreeNode root) {
         func(root);
-
-        if(galat==1){
+        if(galat == 1){
             int temp = galat1_first.val;
-            galat1_first.val=galat1_sec.val;
-            galat1_sec.val=temp;
+            galat1_first.val = galat1_second.val;
+            galat1_second.val = temp;
         }else{
             int temp = galat1_first.val;
-            galat1_first.val=galat2_sec.val;
-            galat2_sec.val=temp;
+            galat1_first.val = galat2_second.val;
+            galat2_second.val = temp;
         }
     }
 }
