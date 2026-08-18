68870032a152a3c4ebbaeda444e341485dcafbf2
Rohit Chandel <rohitchandel2024@gmail.com>
2026-08-18 09:42:30 +0530

    Time: 3 ms (97.99%) | Memory: 46.3 MB (47.28%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.java
+++ 437-path-sum-iii/path-sum-iii.java
@@ -1,18 +1,17 @@
 class Solution {
     int count = 0;
-    private void func(TreeNode root,long currSum,int targetSum,HashMap<Long,Integer> map){
+    void func(TreeNode root, long currSum, int targetSum, HashMap<Long,Integer>map){
         if(root==null) return;
-        currSum+=root.val;
+        currSum += root.val;
         if(map.containsKey(currSum-targetSum))
-        count+=map.get(currSum-targetSum);
+        count +=map.get(currSum-targetSum);
         map.put(currSum,map.getOrDefault(currSum,0)+1);
-        func(root.left,currSum,targetSum,map);
-        func(root.right,currSum,targetSum,map);
+        func(root.left, currSum, targetSum, map);
+        func(root.right, currSum, targetSum, map);
         map.put(currSum,map.get(currSum)-1);
-        return;
     }
     public int pathSum(TreeNode root, int targetSum) {
-        HashMap<Long,Integer> map = new HashMap<>();
+        HashMap<Long,Integer>map = new HashMap<>();
         if(root==null) return 0;
         map.put(0L,1);
         func(root,0,targetSum,map);
