d5164a635f9d7425fdc0713499fefac974db532f
Rohit Chandel <rohitchandel2024@gmail.com>
2026-08-20 12:02:15 +0530

    Time: 4 ms (46.13%) | Memory: 52.4 MB (45.94%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,13 +1,12 @@
 class Solution {
-    int[] x={-1,1,0,0};
-    int[] y={0,0,-1,1};
+    int [] x = {-1,1,0,0};
+    int [] y = {0,0,-1,1};
     boolean valid(int i, int j, int n, int m){
-       return (i >= 0 && i < n && j >= 0 && j < m);
-            
+        return (i>=0 && i<n && j>=0 && j<m);
     }
-    void dfs(char[] [] grid,int n, int m, int i, int j,boolean[][] visit){
-        visit[i][j]=true;
-        for(int k=0;k<4;k++){
+    void dfs(char[][] grid, int n, int m, int i, int j, boolean[][] visit){
+        visit[i][j] = true;
+        for(int k=0; k<4; k++){
             int row = i+x[k];
             int col = j+y[k];
             if(valid(row,col,n,m) && grid[row][col]=='1' && !visit[row][col]){
@@ -18,10 +17,10 @@ class Solution {
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int res=0;
-        boolean [][] visit=new boolean[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        int res = 0;
+        boolean [][] visit = new boolean[n][m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(grid[i][j]=='1' && !visit[i][j]){
                     dfs(grid,n,m,i,j,visit);
                     res++;
