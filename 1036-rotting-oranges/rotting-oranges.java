3ae990c34d99119c7e5207fe274b13ed83ae541a
Rohit Chandel <rohitchandel2024@gmail.com>
2026-08-21 10:34:56 +0530

    Time: 2 ms (89.62%) | Memory: 43.6 MB (98.98%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,45 +1,44 @@
 class Solution {
-    int [] x = {-1,1,0,0};
-    int [] y = {0,0,-1,1};
+    int[] x = {-1,1,0,0};
+    int[] y = {0,0,-1,1};
+    
     boolean valid(int i, int j, int n, int m){
-        return(i>=0 && i<n && j>=0 && j<m);
+        return (i>=0 && i<n && j>=0 && j<m);
     }
-
     public int orangesRotting(int[][] grid) {
-        Queue<int[]>q=new LinkedList<>();
-
+        Queue<int[]>q = new LinkedList<>();
         int n = grid.length;
         int m = grid[0].length;
-        int fresh =0;
-        int time =0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        int fresh = 0;
+        int time = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(grid[i][j]==2){
                     q.add(new int[]{i,j});
                 }
-                else if(grid[i][j]==1){
-                    fresh++;
-                }
+                else if(grid[i][j]==1) fresh++;
             }
         }
-        while(!q.isEmpty()&&fresh>0){
-            time++;
-            int size = q.size();
-            for(int s=0;s<size;s++){
-                int [] p = q.poll();
-                int row =p[0];
-                int col=p[1];
-                for(int k=0;k<4;k++){
-                    int newRow = row+x[k];
-                    int newCol = col+y[k];
-                    if(valid(newRow,newCol,n,m)&&grid[newRow][newCol]==1){
-                        grid[newRow][newCol]=2;
-                        q.add(new int []{newRow,newCol});
-                        fresh--;
+                while(!q.isEmpty() && fresh>0){
+                    time++;
+                    int size = q.size();
+                    for(int s=0; s<size; s++){
+                        int [] p = q.poll();
+                        int row = p[0];
+                        int col = p[1];
+                        for(int k=0; k<4; k++){
+                            int newR = row+x[k];
+                            int newC = col+y[k];
+                            if(valid(newR,newC,n,m) && grid[newR][newC]==1){
+                                grid[newR][newC]=2;
+                                q.add(new int[]{newR,newC});
+                                fresh--;
+                            }
+                        }
                     }
-                }
-            }
+                
+            
         }
-        return (fresh>0) ? -1 : time; 
+        return (fresh>0) ? -1 : time;
     }
 }
