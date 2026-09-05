3bb0c960f143f191d53454c4958b06fd84c249d2
Rohit Chandel <rohitchandel2024@gmail.com>
2026-09-05 09:59:07 +0530

    Time: 54 ms (42.78%) | Memory: 47.7 MB (51.65%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,22 +1,19 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int n = nums.length;
-        int sum=0;
-        for(int i=0; i<n;i++){
-            sum = sum+nums[i];
+        int sum = 0;
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
         }
-         if(sum%2==1) return false;
-        Boolean dp[][] = new Boolean [n+1][sum/2+1];
-        dp[n][0] = true;
-        for(int j =1; j<=sum/2; j++){
-            dp[n][j]= false;
-        }
-        for(int i=n-1; i>=0;i--){
+        if(sum%2==1) return false;
+        boolean [][] dp = new boolean[n+1][sum/2+1];
+        dp[n][0]=true;
+        for(int i=nums.length-1; i>=0; i--){
             for(int j=0; j<=sum/2; j++){
                 if(nums[i]>j){
-                    dp[i][j] = dp[i+1][j];
+                    dp[i][j]=dp[i+1][j];
                 }else{
-                    dp[i][j]= dp[i+1][j-nums[i]] || dp[i+1][j];
+                    dp[i][j]=dp[i+1][j-nums[i]] || dp[i+1][j];
                 }
             }
         }
