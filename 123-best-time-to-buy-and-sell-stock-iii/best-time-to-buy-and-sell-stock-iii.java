fb33040142806af1788fbbdec42930c2b7f75bf2
Rohit Chandel <rohitchandel2024@gmail.com>
2026-09-08 09:52:47 +0530

    Time: 86 ms (27.74%) | Memory: 108.5 MB (42.04%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+      int n = prices.length;
+      int[][] dp = new int[n][5];
+      for(int[] rows : dp){
+        Arrays.fill(rows,-1);
+      }  
+      int k =4;
+      return func(prices,n,0,k,dp);
+    }
+    private int func(int[] prices, int n, int i, int k, int[][] dp){
+        if(i == n ) return 0;
+        if(k==0) return 0;
+        if(dp[i][k]!=-1) return dp[i][k];
+        if(k%2==0) {
+            int c1 = func(prices,n,i+1,k-1,dp)-prices[i];
+            int c2 = func(prices,n,i+1,k,dp);
+             return dp[i][k] = Math.max(c1,c2);
+        }else{
+            int c1 = func(prices,n,i+1,k-1,dp)+prices[i];
+            int c2 = func(prices,n,i+1,k,dp);
+            return dp[i][k] = Math.max(c1,c2);
+        }
+        
+    }
+}
